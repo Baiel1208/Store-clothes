1525778e59dc2cdea1aa27b0094fd4c73f93da57
Baiel1208 <baiel000005@gmail.com>
2023-12-23 18:48:27 +0600

    Тесты для Продукта

--- core/settings.py
+++ core/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
     # OAuth
     'allauth.account.middleware.AccountMiddleware',
 ]
